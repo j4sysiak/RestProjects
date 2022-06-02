6819c1b50ba01698c2db8da7256eecbb30c8aa41
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-02 21:06:56 +0200

    02-06-2022 21:03

---
+++ RestApiBykowski/src/main/java/com/example/RestApiBykowski/RestApiBykowskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiBykowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBykowskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBykowskiApplication.class, args);
+	}
+
+}
---
+++ RestApiBykowski/src/test/java/com/example/RestApiBykowski/RestApiBykowskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestApiBykowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBykowskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
