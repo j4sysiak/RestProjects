4476fe5762a8c14439684eb409681e84ad56dc6e
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-03 17:50:38 +0200

    03-06-2022 17:50

---
+++ rest-api-bykowski-2/src/main/java/com/example/demo/Order.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+
+public class Order {
+    private Customer customer;
+    private Address shippingAddress;
+
+    public Order() {
+    }
+
+    public Order(Customer customer, Address shippingAddress) {
+        this.customer = customer;
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+}
--- rest-api-bykowski-2/src/main/java/com/example/demo/RestApiBykowski2Application.java
+++ rest-api-bykowski-2/src/main/java/com/example/demo/RestApiBykowski2Application.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class RestApiBykowski2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiBykowski2Application.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.addMappings(new PropertyMap<Order, OrderDTO>() {
+			@Override
+			protected void configure() {
+				map().setCity(source.getShippingAddress().getCity());
+				map().setStreet(source.getShippingAddress().getStreet());
+				map().setName(source.getCustomer().getName());
+				map().setLocalDate(LocalDate.now());
+			}
+		});
+
+		return modelMapper;
 	}
 
 }
