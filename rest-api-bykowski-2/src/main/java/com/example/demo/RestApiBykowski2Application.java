7f62c7d455def8d95928e343e891a85f393e4f47
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-03 17:34:23 +0200

    03-06-2022 17:27

---
+++ rest-api-bykowski-2/src/main/java/com/example/demo/RestApiBykowski2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBykowski2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBykowski2Application.class, args);
+	}
+
+}
